de064cc5a3e227dd558e21a4fee420b94aec108b
Xiaoyue Chen <xchen@vvvu.org>
2024-01-20 02:25:59 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  outputs = {self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        packages.default = stdenv.mkDerivation {
+          name = "synth-2009";
+          src = ./.;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp chains synth acm $out/bin
+          '';
+        };
+
+        devShells.default = mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = [ graphviz xdot ];
+        };
+      }
+    );
+}
